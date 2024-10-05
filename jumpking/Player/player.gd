a96a72e8de68952ec37944c1b2018469a9f7ffd2
jumpy <smit@Jumpys-Air.ht.home>
2024-10-04 18:03:03 -0700

    removed mountains fron intro screen, added player animation in intro screen, and linked the play button with a tester world

--- jumpking/Player/player.gd
+++ jumpking/Player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var anim = get_node("AnimationPlayer")
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +15,19 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if direction==-1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction==1:
+		get_node("AnimatedSprite2D").flip_h = false
+		
 	move_and_slide()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
--- jumpking/main.gd
+++ jumpking/main.gd
@@ -1,9 +1,12 @@
 extends Node2D
+@onready var player = $Player
+@onready var anim_player = player.get_node("AnimationPlayer")
 
-
-
+func _ready():
+	anim_player.play("Intro")  
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
-	
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://mechanics_tester.tscn")
