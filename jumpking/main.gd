7367808bc21f338c262afe918a87a39891f09ee1
jumpy <smit@Jumpys-Air.ht.home>
2024-10-04 20:39:37 -0700

    added intro animation and play functionality such that when play pressed, player falls to ground to the new game scene

--- jumpking/main.gd
+++ jumpking/main.gd
@@ -1,6 +1,9 @@
 extends Node2D
+
 @onready var player = $Player
 @onready var anim_player = player.get_node("AnimationPlayer")
+@onready var intro_collision_body = $Player_temp_floor/CollisionShape2D
+@onready var camera = $Player/Camera2D
 
 func _ready():
 	anim_player.play("Intro")  
@@ -9,4 +12,14 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://mechanics_tester.tscn")
+	destroy_collision_body()
+	
+func _process(delta):
+	if player.position.y>2600:
+		get_tree().change_scene_to_file("res://mechanics_tester.tscn")
+
+#destroy introcollision body 
+func destroy_collision_body():
+	if intro_collision_body:
+		intro_collision_body.queue_free() 
+	
